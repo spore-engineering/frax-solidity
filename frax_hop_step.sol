d40efbc5e3a480cf5c304bcc1cba6cec0d4b0a45
Sam Kazemian <8926209+samkazemian@users.noreply.github.com>
2020-08-01 01:02:09 -0700

    Update frax_hop_step.sol

--- frax_hop_step.sol
+++ frax_hop_step.sol
@@ -1,81 +1,113 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.6.0;
 
 import "./SafeMath.sol";
 import "./fxs.sol";
 import "./frax.sol";
-/*
 
-6/30 push
-    -fixed the error in calling FRAX.mint(new_supply) line 37 by implementing a public, non-internal mint() function in FRAXStablecoin
-    -fixed bug in line 73, calls FRAX_price() instead of FRAX_price
-    -kicked the can down the road for line 74 .div(10000)
-    -successfully compiles and deploys now
-*/
-contract frax_hop_step {
- 
-address hopBidder;
-uint256 hopBid;
-address fxs_address;
-address frax_address;
-FRAXShares FXS = FRAXShares(fxs_address); 
-FRAXStablecoin FRAX = FRAXStablecoin(frax_address); 
-uint256 last_hop_time;
+contract fxs_test {
+    FRAXShares FXS;
+    FRAXStablecoin FRAX;
+    
+    function fxs_testt(uint256 fxs_a) public {
+        FXS.burnFrom(msg.sender, fxs_a);
+    }
+    
+}
 
-function fraxHop() public {
-    require(block.timestamp - last_hop_time >= 60);  //3600 seconds means can be called once per hour
+
+contract frax_pool {
+    ERC20 collateral_token;
+    //address collateral_address; //unused
+    //address fxs_address; //unused
+    //address frax_address; //unused
+    //address frax_pool_address; //unused
+    address pool_oracle;
+    FRAXShares FXS;
+    FRAXStablecoin FRAX;
+    uint256 collateral_price_int; //6 decimals of precision, e.g. 1023000 represents $1.023
+    //pool_ceiling is the total amount of collateral that a pool contract can hold
+    uint256 pool_ceiling;
     
-// send previous hop winner their FRAX
-    if (hopBidder != address(0)) {
-        FRAX.transfer(hopBidder, FRAX.balanceOf(address(this))); //what does address(this) return? frax or frax_hop_step?
-        FXS.burn(hopBid);
-        hopBidder = address(0);
-        hopBid = 0;
+        constructor(
+     address _oracle_address) 
+    public 
+    {
+
+    pool_oracle = _oracle_address;
 }
-    
-    // Mint new FRAX
-    //frax doesn't hop to fractionality unless the price is above $1 with this
-    if (FRAX.n_collateral_ratio() <= 1 && FRAX.FRAX_price() > 1e18) {
-        uint256 new_supply = FRAX.totalSupply() / 10000; //MAKE SAFE MATH .div(10000);
-        FRAX.mint(new_supply);
-        last_hop_time = block.timestamp; //set the time of the last expansion
+
+    modifier onlyByOracle() {
+        require(msg.sender == pool_oracle, "you're not the oracle :p");
+        _;
     }
-}
     
-    function bidExpand (uint256 fxs) public {
-        require(fxs > hopBid, "bid is not greater than previous bid");
+    
+    function setPoolCeiling(uint256 new_ceiling) public onlyByOracle {
+        pool_ceiling = new_ceiling;
+    }
+
+    function setOracle(address new_oracle) public onlyByOracle {
+        pool_oracle = new_oracle;
+    }
+    
+    function setCollateralAdd(address collateral_address) public onlyByOracle {
+        collateral_token = ERC20(collateral_address);
+    }
+    
+    function setFRAXAddress(address frax_contract_address) public onlyByOracle {
+        FRAX = FRAXStablecoin(frax_contract_address);
+    }
+    
+    function setPrice(uint256 c_price) public onlyByOracle {
+        collateral_price_int = c_price;
+    }
+    
+
+
+    
+    function mintFRAX(uint256 collateral_amount, uint256 FXS_amount) public payable {
+        //since solidity truncates division, every divsion operation must be the last operation in the equation to ensure minimum error
         
-        // refund previous bidder
-    if (hopBidder != address(0)) {
-    FXS.transfer(hopBidder, hopBid);
+        uint256 fxs_needed;
+        uint256 collateral_needed;
         
-    }
+        //if fully decollateralized
+        if(FRAX.global_collateral_ratio() == 0){ //check here or else calculating c_amount throws dividebyzero error
+            FXS.transferFrom(msg.sender, address(this), FXS_amount);
+            FRAX.pool_mint(msg.sender, FXS_amount);
+            FXS.burnFrom(msg.sender, FXS_amount);
+            return;
+        }
         
-        // record new bidder
-        hopBidder = msg.sender;
-        FXS.transferFrom(msg.sender, address(this), fxs);
+        uint256 fxs_price = FRAX.FXS_price();
+        uint256 col_price = collateral_price_int;
+        uint256 col_ratio = FRAX.global_collateral_ratio();
+        
+        uint256 c_amount = (collateral_amount * col_price) / col_ratio; //replace with safemath .div()
+        uint256 f_amount = (FXS_amount * fxs_price) / (1e6 - col_ratio); //fxs_price has 6 extra precision, col_ratio also has 6 extra precision; replace with safemath .div()
+        
+        
+        if (c_amount < f_amount) {
+            collateral_token.transferFrom(msg.sender, address(this), collateral_amount);
+            fxs_needed = (c_amount * (1e6 - col_ratio)) / 1e6;
+            FRAX.pool_mint(msg.sender, collateral_amount);
+            FXS.burnFrom(msg.sender, fxs_needed);
+        }
+        
+        else {
+            collateral_needed = (f_amount * col_ratio) / 1e6;
+            collateral_token.transferFrom(msg.sender, address(this), collateral_needed);
+            FRAX.pool_mint(msg.sender, f_amount); 
+            FXS.burnFrom(msg.sender, FXS_amount);
+        }
     }
-    
-    address backHopBidder;
-    uint256 backHopBid;
-    uint256 backHopAmount;
-    uint256 last_back_hop_time;
-    
-    function fraxBackstep() public {
-    require(block.timestamp - last_back_hop_time >= 60); //3600 seconds means can be called once per hour
-    
-    // send previous hop winner their FXS
-    if (backHopBidder != address(0)) {
-        FXS.mint(backHopBidder, backHopBid);
-        backHopBidder = address(0);
-        backHopBid = 0;
-        backHopAmount = 0;
+
+    function redeemFRAX(uint256 FRAX_amount) public payable {
+        uint col_ratio = FRAX.global_collateral_ratio();
+        collateral_token.transferFrom(address(this), msg.sender, (FRAX_amount * (1e6 - col_ratio)) / 1e6); 
+        FXS.pool_mint(tx.origin, (FRAX_amount * (1e6 - col_ratio)) / 1e6);
+        FRAX.burnFrom(msg.sender, FRAX_amount);
     }
-    
-    // Start contraction
-    if (FRAX.n_collateral_ratio() > 1 && FRAX.FRAX_price() < 1e18) {
-        backHopAmount = FRAX.totalSupply() / 10000; // MAKE SAFE MATH .div(10000)
-        last_back_hop_time = block.timestamp; //set the time of the last contraction
-    }
-}   
+
     
 }
