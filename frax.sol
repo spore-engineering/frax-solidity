ce56bff269ba9833a642dd09f11f74ac872f26e2
Sam Kazemian <8926209+samkazemian@users.noreply.github.com>
2020-08-30 02:12:23 -0700

    Update frax.sol

--- frax.sol
+++ frax.sol
@@ -4,6 +4,7 @@ import "./Context.sol";
 import "./IERC20.sol";
 import "./SafeMath.sol";
 import "./fxs.sol";
+import "./frax_pool.sol";
 
 
 
@@ -25,17 +26,15 @@ contract FRAXStablecoin is ERC20 {
     //add other future monetary policy contracts to this
     mapping(address => bool) public frax_monetary_policy_contracts;
 
-    
-
     uint256 public FRAX_price; //6 decimals of precision
     uint256 public FXS_price; //6 decimals of precision
     uint256 public global_collateral_ratio; //6 decimals of precision, e.g. 924102 = 0.924102
     address oracle_address; 
-    uint256 public redemption_fee;
-    uint256 public minting_fee;
+    uint256 public redemption_fee; //6 decimals of precision, divide by 1000000 in calculations for fee
+    uint256 public minting_fee; //6 decimals of precision, divide by 1000000 in calculations for fee
 
     modifier onlyMonPol() {
-       require(frax_monetary_policy_contracts[msg.sender] == true, "only frax expansion-retraction contracts can use this!");
+       require(frax_monetary_policy_contracts[msg.sender] == true, "only frax monetary policy contracts can use this!");
         _;
     } 
      
@@ -64,9 +63,14 @@ contract FRAXStablecoin is ERC20 {
         _mint(msg.sender, amount);
     }
 
-    //used by pools when user redeems1t1
+    //used by pools when user redeems
     function poolBurn(uint256 amount) public onlyPools {
         _burn(tx.origin,amount);
+    }
+
+    //this function is what other frax pools will call to mint new FRAX 
+    function pool_mint(address m_address, uint256 m_amount) public onlyPools {
+        super._mint(m_address, m_amount);
     }
 
     //adds collateral addresses supported, such as tether and busd, must be ERC20 
@@ -74,6 +78,8 @@ contract FRAXStablecoin is ERC20 {
         frax_pools[pool_address] = true; 
         frax_pools_array.push(pool_address);
     }
+
+
 
     //adds the monetary policy contracts, hop, backstep etc 
     function setMonetaryPolicyContract(address con_address) public onlyByOracle {
@@ -89,6 +95,44 @@ contract FRAXStablecoin is ERC20 {
         FRAX_price = FRAX_p;
         FXS_price = FXS_p;
     }
+    
+
+    //iterate through all frax pools and calculate all value of collateral in all pools globally 
+    function globalCollateralValue() public view returns (uint256) {
+        uint256 pool_value;
+        uint256 total_collateral_value; 
+        
+        for (uint i=0; i<frax_pools_array.length; i++){
+            ERC20 pool_token = ERC20(frax_pools_array[i]);
+            pool_value = pool_token.balanceOf(frax_pools_array[i]) * frax_pool(frax_pools_array[i]).collateral_price_int();
+            total_collateral_value = total_collateral_value + pool_value;
+   
+    }
+    return total_collateral_value;
+}
+    
+    //there needs to be a time interval that this can be called. Otherwise it can be called multiple times per expansion.
+    uint256 last_call_time; //last time the setNewCollateralRatio function was called
+    function setNewCollateralRatio() public {
+        require(block.timestamp - last_call_time >= 3600 && FRAX_price != 1000000);  //3600 seconds means can be called once per hour, 86400 seconds is per day, callable only if FRAX price is not $1
+        
+        uint256 tot_collat_value =  globalCollateralValue();
+        uint256 globalC_ratio = totalSupply().div(tot_collat_value); //div by 12 places, FRAX has 18 precision and c_ratio has 6              
+        
+        //step increments are .5% 
+        if (FRAX_price > 1000000) {
+            global_collateral_ratio = globalC_ratio - 5000;
+        }    
+        else {
+            global_collateral_ratio = globalC_ratio + 5000;
+        }
+    last_call_time = block.timestamp; //set the time of the last expansion
+            
+    }
+        
+
+    
+
     
     function setGlobalCollateralRatio(uint256 coll_ra) public onlyByOracle {
         require(coll_ra < 1000001, "collateral ratio must have 6 decimals of precision and never go above 1.0000000");
@@ -108,10 +152,7 @@ contract FRAXStablecoin is ERC20 {
     }  
     
     
-    //this function is what other frax pools will call to mint new FRAX 
-    function pool_mint(address m_address, uint256 m_amount) public onlyPools {
-        super._mint(m_address, m_amount);
-    }
+
     
 
 
